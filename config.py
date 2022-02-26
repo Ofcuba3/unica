52f88b38a18544c70586e21707b1fbab25d72dd2
Ofcuba3 <100317332+Ofcuba3@users.noreply.github.com>
2022-02-26 18:47:21 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from MoodleClient import MoodleClient
 
 BOT_TOKEN = '5250914184:AAEdINQA6hSN4jgGF2QBjaZlz2y3RNiTPQ8'
 MAX_ZIP_SIZE = 80
-ACCES_USERS = ['danyalehs00']
+ACCES_USERS = ['marlonviral']
 CREDENTIALS = {'username':'german45','password':'German69*'}
 CACHE = {}
 
